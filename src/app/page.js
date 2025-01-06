ed1ba8837fe4f661e916786cd1a770b90b5ee479
WebMohit <2003mohitkhemani@gmail.com>
2025-01-07 00:53:43 +0530

    AboutMe

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 
+import AboutMe from "../components/AboutMe";
 import HeroSection from "../components/HeroSection";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection/>
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image";
+
+
+
+const AboutMe = () => {
+  return (
+    <div className="AboutMe">
+   
+      <div className="image">
+        <Image
+          src="/myPicture.jpg"
+          alt="Profile Picture"
+          width={200}
+          height={200}
+          className="profile-image"
+        />
+      </div>
+
+    
+      <div className="text">
+        <h2>About Me</h2>
+        <p>
+          My name is Mohit Khemani. I'm pursuing Bachelors of Computer Application  from Indra Gandhi Open University at New Delhi . I am passionate to develop new things and explore technology
+          and my goal is to pursue this passion within the field of software
+          engineering. In my free time, I like learning new technologies.
+        </p>
+
+
+        <button className="resume-button">Resume</button>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
